ee362f9a859e98a0cf1d13b5a7c7279f95aac731
Zyam-1 <zyammaqsood7@gmail.com>
2025-03-07 23:22:07 +0500

    Implement functionality to get product

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const Blog = require("../../models/Blog");
-const mongoose = require("mongoose");
-
+// const mongoose = require("mongoose");
+const {isObjectIdOrHexString} = require("mongoose");
 
 router.get("/", async (req, res)=>{
-    let blogs = await Blog.find();
-    return res.send(blogs);
+    try {
+        let blogs = await Blog.find().populate("comments").populate("author");
+        return res.send(blogs);
+    } catch (error) {
+        return res.send(error);
+    }
+
+});
+
+router.get("/:id", async(req, res)=>{
+    let _id = req.params.id;
+    // to check if the Object ID Is valid or not
+    // try catch can also be used
+    if(isObjectIdOrHexString(_id)){
+        let blog = await Blog.find({_id: _id}).populate("comments").populate("author");
+        if(blog.length == 0){
+            return res.status(404).send(["Blog Not Found"]);
+        }
+        return res.send(blog);
+    }else{
+        return res.status(400).send(["Invalid ID format"]);
+    }
+
+})
+
+router.post("/", async(req, res)=>{
+    console.log(req);
 })
 
 module.exports = router;
