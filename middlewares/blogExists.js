2afcf17b107872252d0c865d744ccb8f3a4b41fc
Zyam-1 <zyammaqsood7@gmail.com>
2025-03-10 03:36:39 +0500

    Change in comment post route
    Change the comment post route to check if the blog exists and insert the comment id in the respective blog comment as well.

--- middlewares/blogExists.js
+++ middlewares/blogExists.js
@@ -4,10 +4,10 @@ async function blogExists(req, res, next) {
   let blogID = req.params.id;
 
   if (!blogID) {
-    return res.status(400).send("Blog ID is required.");
+    return res.status(400).send(["Blog ID is required."]);
   }
   if (!isObjectIdOrHexString(blogID)) {
-    return res.status(400).send("Invalid Blog ID.");
+    return res.status(400).send(["Invalid Blog ID."]);
   }
 
   let blog = await Blog.findOne({ _id: blogID });
--- routes/api/comments.js
+++ routes/api/comments.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { isObjectIdOrHexString } = require("mongoose");
 const Comment = require("../../models/Comment");
 const blogExists = require("../../middlewares/blogExists");
+const Blog = require("../../models/Blog");
 
 // get all comments from blog id
 router.get("/:id", async (req, res) => {
@@ -63,6 +64,7 @@ router.post("/:id", blogExists, async (req, res) => {
     comment.user = req.body.user_id;
     comment.content = req.body.content;
     await comment.save();
+    await Blog.findOneAndUpdate({ _id }, { $push: { comments: comment._id } });
     await comment.populate("user");
     return res.status(201).send(comment);
   } catch (error) {
